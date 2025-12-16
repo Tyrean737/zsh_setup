94c3f84440e7d10420f8828f05c09732308ef7bc
Tyrean737 <>
2025-12-16 14:18:43 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# zsh_setup
-Used to set up zsh
+# zsh Set Up
+Small script to set up zsh configuration.
+
+### Install
+Just run
+```bash
+sudo bash zsh_setup.sh
+```
+
+and the script installs:
+- zsh
+- woff2-font-awesome (Font Awesome for Emoji support)
+- ttf-hack-nerd (Nerd Font for Unicode support)
+- oh-my-zsh-git
+- zsh-theme-powerlevel10k
+
+And copies the config.zsh to the correct location.
+The config is copied from CachyOS.
+
+### Customize
+On first startup of zsh p10k will run a custimization script. To rerun this script later run
+```zsh
+p10k configure
+```
+
+or edit ~/.p10k.zsh
---
+++ zsh_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Make sure script is not run as root
+if [[ "$EUID" -eq 0 ]]; then
+  echo "This script must NOT be run as root." >&2
+  exit 1
+fi
+
+# Get user name
+USER=$(whoami)
+
+# Install packages
+sudo pacman -S zsh fzf woff2-font-awesome ttf-hack-nerd
+trizen -S --noedit oh-my-zsh-git zsh-theme-powerlevel10k
+
+# Copy config
+mkdir /home/$USER/.config/zsh-config/
+cp config.zsh /home/$USER/.config/zsh-config/
+
+# Copy plugins to zsh directory
+sudo cp -r plugins/ /usr/share/zsh/
+
+# Set up .zshrc
+echo "source /home/$USER/.config/zsh-config/config.zsh" >> /home/$USER/.zshrc
